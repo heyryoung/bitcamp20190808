cd62eb7414013bbaf33356a04cc11c060012e0f7
leja0418 <leja0418@gmail.com>
2019-08-09 15:52:15 +0900

    190809 15:52

--- javabasic/Whatisjava.java
+++ javabasic/Whatisjava.java
@@ -2,6 +2,9 @@ class Whatisjava{
 
     public static void main(String[] args) {
         System.out.print("Hello");
+        System.out.println("What is Your Name");
+
+        System.out.println("My name is "); 
     }
 
 }
