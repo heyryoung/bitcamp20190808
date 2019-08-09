6e62258ed639ab70d34bf0e5d1bf63d55bc154ba
leja0418 <leja0418@gmail.com>
2019-08-09 17:44:02 +0900

    190809 17:44

--- javabasic/Whatisjava.java
+++ javabasic/Whatisjava.java
@@ -1,12 +1,13 @@
 class Whatisjava{
 
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("What is Your Name");
+        System.out.println("안녕");
+        System.out.println("너의 이름이 뭐니?");
 
         String name = "";
-        name = "Kwon";
-        System.out.println("My name is " + name); 
+        name = "알거없잖아";
+        int age = 0;
+        System.out.println("내 이름은 " + name + "이고, 나이는 " + age + "란다" ); 
     }
 
 }
